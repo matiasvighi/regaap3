f3446efb13c3723d37a4844b2a6d2f5cd3c36a65
Matias <matias.vighi@gmail.com>
2021-11-12 18:10:40 -0300

    se pasó toda la lógica de los botones a eventos. (Falta hacer cosas) revisar línea 67 y 78 88 98 etc

--- src/components/TiempoRegado.js
+++ src/components/TiempoRegado.js
@@ -14,12 +14,19 @@ export default function TiempoRegado() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [anchorEl2, setAnchorEl2] = React.useState(null);
   const [field, setField] = React.useState(false);
-  const [aspersores, setAspersores] =React.useState({aspersor1: "Aspersor1",
-  aspersor2: "Aspersor2",
-  aspersor3: "Aspersor3",
-  aspersor4: "Aspersor4",
-  aspersor5: "Aspersor5",
-  aspersor6: "Aspersor6",})
+  const [aspersores, setAspersores] =React.useState([
+  {aspersor1: 1, tiempo: 5},
+  {aspersor2: 2, tiempo: 5},
+  {aspersor3: 3, tiempo: 5},
+  {aspersor4: 4, tiempo: 5},
+  {aspersor5: 5, tiempo: 5},
+  {aspersor6: 6, tiempo: 5}])
+  const [currentMenu, setCurrentMenu] =React.useState({
+    menu:0,
+    submenu:0
+
+  }
+  )
   const open = Boolean(anchorEl);
   const open2 = Boolean(anchorEl2);
   var ispressed = Boolean(field)
@@ -29,13 +36,19 @@ export default function TiempoRegado() {
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
     console.log(event.currentTarget);
-    console.log("target name", event.currentTarget.id);
+    console.log("target name1", event.currentTarget.id);
+    const numberMenu = Number(event.currentTarget.id)
+    //aca poner una logica que extraiga el número de menú y lo guarde en el estado acontinuación:
+    setCurrentMenu({...currentMenu, menu : numberMenu})
   };
   const handleClick2 = (event) => {
     console.log("submenu2"),
       setAnchorEl2(event.currentTarget),
       console.log(event.currentTarget);
-    console.log("target name", event.currentTarget.id);
+    console.log("target name2", event.currentTarget.id);
+    const numberSubMenu = Number(event.currentTarget.id)
+    //aca poner una logica que extraiga el número de submenú y lo guarde en el estado acontinuación:
+    setCurrentMenu({...currentMenu, submenu : numberSubMenu})
   };
   const handleClick3 = (event) => {
     setField(true),
@@ -45,7 +58,14 @@ export default function TiempoRegado() {
 
   };
   
-  const handleChange = (event) => {setName(event.target.value),console.log(name,"nombre2"),setAspersores({...aspersores, aspersor1: name}),console.log(aspersores)};
+  const handleChange = (event) => {
+ console.log("numero menú",currentMenu.menu)
+    
+ const garlopa = currentMenu.menu
+    
+    setName(event.target.value),console.log(name,"nombre2"),setAspersores({...aspersores, garlopa: name}),console.log(aspersores)};
+   //ver como embocar lo de ...aspersores, ksks (ya que ahora están dentro de un array, justamente el n del array es el n, por eso no habria que hacer magias raras)
+    //preguntar como verga se hace para "llamar a una variable cuyo nombre voy a saber más adelante en js"(promise)
   const handleClose = () => {
     setAnchorEl(null);
     setAnchorEl2(null);
@@ -55,17 +75,17 @@ export default function TiempoRegado() {
   return (
     <div>
       <Button
-        id="basic-button1"
+        id="1"
         variant="outlined"
         aria-controls="basic-menu"
         aria-haspopup="true"
         aria-expanded={open ? "true" : undefined}
         onClick={handleClick}
       >
-        {aspersores.aspersor1}
+        {aspersores[0].aspersor1}
       </Button>
       <Button
-        id="basic-button2"
+        id="2"
         variant="outlined"
         aria-controls="basic-menu"
         aria-haspopup="true"
@@ -75,7 +95,7 @@ export default function TiempoRegado() {
         {aspersores.aspersor2}
       </Button>
       <Button
-        id="basic-button3"
+        id="3"
         variant="outlined"
         aria-controls="basic-menu"
         aria-haspopup="true"
@@ -86,7 +106,7 @@ export default function TiempoRegado() {
       </Button>
 
       <Button
-        id="basic-button4"
+        id="4"
         variant="outlined"
         aria-controls="basic-menu"
         aria-haspopup="true"
